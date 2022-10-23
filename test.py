9efdf8786f11501a3a66a4870afe692bc23469ab
Florian Niedermeier <florian-niedermeier@gmx.de>
2022-10-23 21:02:17 +0200

    ádded capability to interact with REST

--- test.py
+++ test.py
@@ -1,16 +1,58 @@
 import hashlib
 import base64
+import json
+import os
+from http.cookiejar import CookieJar, LWPCookieJar
+
 import requests
 
 username = 'florian-niedermeier@gmx.de'
 password = '1R4ciNg_2.4?2'
+
 
 def encode_pw(username, password):
     initialHash = hashlib.sha256((password + username.lower()).encode('utf-8')).digest()
     hashInBase64 = base64.b64encode(initialHash).decode('utf-8')
     return hashInBase64
 
+
+def authenticate(pwValueToSubmit):
+    loginAdress = "https://members-ng.iracing.com/auth"
+    loginHeaders = {"Content-Type": "application/json"}
+    authBody = {"email": username, "password": pwValueToSubmit}
+
+    session = requests.Session()
+    cookie_File = "cookie-jar.txt"
+    session.cookies = LWPCookieJar(cookie_File)
+
+    if not os.path.exists(cookie_File):
+        print('setting cookies')
+        session.cookies.save()
+        loginNow = session.post(loginAdress, json=authBody, headers=loginHeaders)
+        response_Data = loginNow.json()
+
+        if loginNow.status_code == 200 and response_Data['authcode']:
+            if cookie_File:
+                session.cookies.save(ignore_discard=True)
+            authenticated = True
+        else:
+            raise RuntimeError("Error from iRacing: ", response_Data)
+
+    else:
+        print('loading saved cookies')
+        session.cookies.load(cookie_File)
+
+    return session
+
+
+###############################
+
 pwValueToSubmit = encode_pw(username, password)
+session = authenticate(pwValueToSubmit)
 
-print(f'{username}\n{pwValueToSubmit}')
+responseJson = session.get('https://members-ng.iracing.com/data/car/get')
+responseDict = responseJson.json()
+finalJson = requests.get(responseDict["link"])
+finalList = finalJson.json()
 
+print(finalList[0]["car_name"])
