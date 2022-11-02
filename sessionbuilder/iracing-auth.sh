6338b83e8d7e3fe106cf0d1dc07ae958723787e6
Florian Niedermeier <florian-niedermeier@gmx.de>
2022-11-02 01:35:43 +0100

    - worked on delta-diagram - caching now for fuzzwah-api as well

--- sessionbuilder/iracing-auth.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-EMAIL="florian-niedermeier@gmx.de"
-PASSWORD="1R4ciNg_2.4?2"
-EMAILLOWER=$(echo -n "$EMAIL" | tr [:upper:] [:lower:])
-ENCODEDPW=$(echo -n $PASSWORD$EMAILLOWER | openssl dgst -binary -sha256 | openssl base64)
-BODY="{\"email\": \"$EMAIL\", \"password\": \"$ENCODEDPW\"}"
-/usr/bin/curl -c cookie-jar.txt -X POST -H 'Content-Type: application/json' --data "$BODY" https://members-ng.iracing.com/auth
