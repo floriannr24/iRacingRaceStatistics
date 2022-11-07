4a700e3a4e76af27d7d6b8f8003d97fb51c6d4d6
Florian Niedermeier <florian-niedermeier@gmx.de>
2022-11-07 23:38:10 +0100

    - added DiagramBase (class to inherit from)

---
+++ diagram/diagram_base.py
@@ -0,0 +1,26 @@
+from matplotlib import pyplot as plt
+
+
+class DiagramBase:
+
+    def __init__(self, px_width, px_height, input):
+        self.input = input
+        self.fig, self.ax = plt.subplots(nrows=1, ncols=1, figsize=(px_width / 100, px_height / 100))
+
+        # define color scheme
+        self.ax_color = "#2F3136"
+        self.ax_gridlines_color = "#3F434A"
+        self.ax_spines_color = "#3F434A"
+        self.ax_tick_color = "#C8CBD0"
+        self.fig_color = "#36393F"
+
+        # set colors & formatting
+        self.ax.set_facecolor(self.ax_color)
+        self.ax.grid(color=self.ax_gridlines_color, axis="y", zorder=0)
+        self.ax.spines["left"].set_color(self.ax_spines_color)
+        self.ax.spines["bottom"].set_color(self.ax_spines_color)
+        self.ax.spines["right"].set_color(self.ax_spines_color)
+        self.ax.spines["top"].set_color(self.ax_spines_color)
+        self.ax.tick_params(axis="x", colors=self.ax_tick_color)
+        self.ax.tick_params(axis="y", colors=self.ax_tick_color)
+        self.fig.set_facecolor(self.fig_color)
