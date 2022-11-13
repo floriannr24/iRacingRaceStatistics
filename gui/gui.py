e284e9c41ed04877b7b5559362239a470f151354
Florian Niedermeier <florian-niedermeier@gmx.de>
2022-11-13 18:50:40 +0100

    - gui updated (tree view, button/checkbox functionality...)

--- gui/gui.py
+++ gui/gui.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from data.recent_races import RecentRaces
-from helpers.diagram import Diagram, Configurator, ConfiguratorTest
+from helpers.diagram import Diagram, Configurator
 from sessionbuilder.session_builder import SessionBuilder
 
 
@@ -20,51 +20,75 @@ class GUI:
         frame_top = tk.Frame(self.root)
         frame_top.pack(side="top", fill="x")
 
-        frame_top.columnconfigure(0, weight=1)
+        frame_top.columnconfigure(0, weight=0)
         frame_top.columnconfigure(1, weight=5)
 
         top_label = ttk.Label(frame_top, text="Choose race session", font=("Arial", "14"))
-        top_label.grid(column=0, row=0, padx=10, pady=10, sticky="w")
+        top_label.grid(column=0, row=0, padx=10, pady=10, sticky="w", columnspan=2)
 
         r1 = ttk.Radiobutton(frame_top, text="Race session:", variable=self.radio, value=0, command=self.enableEntry1)
         r1.grid(column=0, row=1, sticky="w", padx=10, pady=3)
 
-        self.entry1 = ttk.Combobox(frame_top, textvariable=self.entry1_index)
-        self.entry1.grid(column=1, row=1, sticky="we", padx=(0, 10), pady=3)
-        self.entry1["state"] = "readonly"
-        self.entry1["values"] = self.fillEntry1()
-        self.entry1.current(0)
+        columnsTree = ("time", "series", "track", "winner", "sof", "subsession_id")
+        self.tree = ttk.Treeview(frame_top, columns=columnsTree, show="headings", height=5)
+        self.tree.heading("time", text="Time")
+        self.tree.heading("series", text="Series")
+        self.tree.heading("track", text="Track")
+        self.tree.heading("winner", text="Winner")
+        self.tree.heading("sof", text="SOF")
+        self.tree.heading("subsession_id", text="Subsession ID")
+        self.tree.grid(column=1, row=1, sticky="we")
+
+        self.tree.column("time", width=140)
+        self.tree.column("winner", width=110)
+        self.tree.column("sof", width=70)
+        self.tree.column("subsession_id", width=90)
+
+
+        self.fillTree()
+
+        scrollbar = ttk.Scrollbar(frame_top, orient=tk.VERTICAL, command=self.tree.yview)
+        self.tree.configure(yscrollcommand=scrollbar.set)
+        scrollbar.grid(column=2, row=1, sticky="ns")
+
+
+        # self.entry1 = ttk.Combobox(frame_top, textvariable=self.entry1_index, width=110)
+        # self.entry1.grid(column=1, row=1, sticky="we", padx=(0, 10), pady=3)
+        # self.entry1["state"] = "readonly"
+        # self.entry1["values"] = self.fillEntry1()
+        # self.entry1.current(0)
 
         r2 = ttk.Radiobutton(frame_top, text="Subsession ID:", variable=self.radio, value=1, command=self.enableEntry2)
         r2.grid(column=0, row=2, sticky="w", padx=10, pady=3)
 
         self.entry2 = ttk.Entry(frame_top)
-        self.entry2.grid(column=1, row=2, sticky="we", padx=(0, 10), pady=3)
+        self.entry2.grid(column=1, row=2, sticky="w", padx=(0, 10), pady=3)
         self.entry2.config(state="disabled")
 
     def enableEntry1(self):
-        self.entry1.config(state="enabled")
         self.entry2.config(state="disabled")
 
     def enableEntry2(self):
-        self.entry1.config(state="disabled")
         self.entry2.config(state="enabled")
 
-    def fillEntry1(self):
+    def fillTree(self):
         my_sessionBuilder = SessionBuilder()
         my_sessionBuilder.authenticate()
         session = my_sessionBuilder.session
 
-        self.entry1_data = RecentRaces(session).get_RecentRaces_Data()
+        self.tree_data = RecentRaces(session).get_RecentRaces_Data()
 
-        entry_values = []
+        tree_values = []
 
-        for x in self.entry1_data:
-            entry_values.append(
-                f"{x['session_start_time']} / {x['series_name']} / {x['track_name']} / {x['winner_name']} / {x['SOF']} / {x['subsession_id']}")
+        for x in self.tree_data:
+            tree_values.append(
+                (f"{x['session_start_time']}", f"{x['series_name']}", f"{x['track_name']}", f"{x['winner_name']}", f"{x['SOF']}", f"{x['subsession_id']}")
+            )
 
-        return entry_values
+        for data in tree_values:
+            self.tree.insert("", tk.END, values=data)
 
+        return tree_values
 
 class TabONE(GUI):
     def __init__(self):
@@ -101,40 +125,41 @@ class TabONE(GUI):
 
         frame_notebook.pack(side="left", expand=1, fill="x")
 
-        self.tab1.columnconfigure(0, weight=1)
-        self.tab1.columnconfigure(1, weight=3)
-        self.tab1.columnconfigure(2, weight=1)
-        self.tab1.columnconfigure(3, weight=1)
+        # self.tab1.columnconfigure(0, weight=1)
+        # self.tab1.columnconfigure(1, weight=3)
+        # self.tab1.columnconfigure(2, weight=1)
+        # self.tab1.columnconfigure(3, weight=1)
 
         self.optional_cb = ttk.Checkbutton(self.tab1, text="Options:", variable=self.optional,
                                            command=self.activateOptions)
         self.optional_cb.grid(column=1, row=0)
 
-        self.setYAxis_cb = ttk.Checkbutton(self.tab1, text="Set y-axis:", variable=self.setYMinMax_val)
-        self.setYAxis_cb.grid(column=1, row=1, sticky="w")
+        self.setYMinMax_cb = ttk.Checkbutton(self.tab1, text="Set y-axis:", variable=self.setYMinMax_val)
+        self.setYMinMax_cb.grid(column=1, row=1, sticky="w")
         self.ymin_entry = ttk.Entry(self.tab1)
-        self.ymin_entry.grid(column=2, row=1)
+        self.ymin_entry.grid(column=2, row=1, sticky="w")
         self.ymax_entry = ttk.Entry(self.tab1)
-        self.ymax_entry.grid(column=3, row=1)
+        self.ymax_entry.grid(column=3, row=1, sticky="w")
 
-        self.setYAxisInterval_cb = ttk.Checkbutton(self.tab1, text="Set y-axis interval:",
-                                                   variable=self.setYInterval_val)
+        self.setYAxisInterval_cb = ttk.Checkbutton(self.tab1, text="Set y-axis interval:", variable=self.setYInterval_val)
         self.setYAxisInterval_cb.grid(column=1, row=2, sticky="w")
         self.yAxisInterval_combo = ttk.Combobox(self.tab1)
-        self.yAxisInterval_combo.grid(column=2, row=2)
+        self.yAxisInterval_combo.grid(column=2, row=2, sticky="w")
         self.yAxisInterval_combo["state"] = "readonly"
         self.yAxisInterval_combo["values"] = ("2.0", "1.0", "0.5", "0.25")
         self.yAxisInterval_combo.current(2)
 
         self.showDISCDISQ_cb = ttk.Checkbutton(self.tab1, text="Show DISC / DISQ drivers", variable=self.DISQDISC_val)
         self.showDISCDISQ_cb.grid(column=1, row=3, sticky="w")
+
         self.showDots_cb = ttk.Checkbutton(self.tab1, text="Show individual laptimes", variable=self.dots_val)
         self.showDots_cb.grid(column=1, row=4, sticky="w")
-        self.showMean_cb = ttk.Checkbutton(self.tab1, text="Show mean", variable=self.mean_val).grid(column=1, row=5,
-                                                                                                     sticky="w")
 
-        self.start_button_bpm = tk.Button(self.tab1, text="Start", command=self.start_bpm).grid(column=0, row=0,
-                                                                                                sticky="we")
+        self.showMean_cb = ttk.Checkbutton(self.tab1, text="Show mean", variable=self.mean_val)
+        self.showMean_cb.grid(column=1, row=5, sticky="w")
+
+        self.start_button_bpm = tk.Button(self.tab1, text="Start", command=self.start_bpm)
+        self.start_button_bpm.grid(column=0, row=0,sticky="we")
 
         self.root.mainloop()
 
@@ -158,20 +183,20 @@ class TabONE(GUI):
         for key, value in config.options.items():
             print(f" Key: {key} / Value: {value} ")
 
-        if self.radio.get() == 0:
-            index = self.entry1.current()
-            subsession_id = self.entry1_data[index]["subsession_id"]
-            Diagram(subsession_id, session, Configurator("bpm"))
-
-
-        elif self.radio.get() == 1:
-            Diagram(self.entry2.get(), session, Configurator("bpm"))
+        # if self.radio.get() == 0:
+        #     index = self.entry1.current()
+        #     subsession_id = self.entry1_data[index]["subsession_id"]
+        #     Diagram(subsession_id, session, Configurator("bpm"))
+        # if self.radio.get() == 1:
+        #     Diagram(self.entry2.get(), session, Configurator("bpm"))
 
     def packConfig(self):
         return Configurator("bpm",
                             setYAxis=self.setYMinMax_val.get(),
                             minVal=self.ymin_entry.get(),
                             maxVal=self.ymax_entry.get(),
+                            setYAxsisInterval=self.setYInterval_val.get(),
+                            interval=self.yAxisInterval_combo.get(),
                             showDISC=self.DISQDISC_val.get(),
                             showLaptimes=self.dots_val.get(),
                             showMean=self.mean_val.get()
